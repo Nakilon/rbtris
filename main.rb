a77761cbbeeb7cc0955e839ce7638d157e3cec61
Victor Maslov <nakilon@gmail.com>
2019-05-09 23:50:48 +0300

    treating the cancer of OOP; deprecated controller support

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require "ruby2d"
 
-Wait = 18
+wait = 18
 
 class Tetromino
   attr_accessor :x, :y
@@ -43,169 +43,158 @@ class Tetromino
   end
 end
 
-class Field
-  Width, Height = 10, 20
 
-  Margin = 30
-  BlockMargin = 1
-  BlockSide = 25
+  width, height = 10, 20
 
-  attr_accessor :key_in
+  block_side = 25
 
-  def initialize
-    s = BlockMargin * 2 + BlockSide
-    w = Margin * 2 + s * Width
-    h = Margin * 2 + s * Height
-
+key_in = nil
+blocks = nil
+field = nil
+init = lambda do
+    block_margin = 1
+    margin = 30
+    s = block_margin * 2 + block_side
+    w = margin * 2 + s * width
+    h = margin * 2 + s * height
     Window.set width: w, height: h, title: "rbTris"
     Rectangle.new x: 0,      y: 0,      width: w,              height: h,              color: "#ABF8FC"
-    Rectangle.new x: Margin, y: Margin, width: w - 2 * Margin, height: h - 2 * Margin, color: "black"
+    Rectangle.new x: margin, y: margin, width: w - 2 * margin, height: h - 2 * margin, color: "black"
 
-    @blocks = Array.new(Height) do |y|
-      Array.new(Width) do |x|
-        Square.new x: Margin + BlockMargin + s * x,
-                   y: Margin + BlockMargin + s * y,
-                   size: BlockSide, color: "red", z: 10
+    blocks = Array.new(height) do |y|
+      Array.new(width) do |x|
+        Square.new x: margin + block_margin + s * x,
+                   y: margin + block_margin + s * y,
+                   size: block_side, color: "red", z: 10
       end
     end
-    @field = Array.new(Height){ Array.new(Width){ 0 } }
+    field = Array.new(height){ Array.new(width){ 0 } }
 
-    @key_in = true
+    key_in = true
   end
 
-  def render
-    Height.times do |y|
-      Width.times do |x|
-        @blocks[y][x].color = %w{ #158FAC #F1F101 #2FFF43 #DF0F0F #5858FF #FFB950 #FF98F3 }[@field[y][x] - 1]
-        @field[y][x].zero? ? @blocks[y][x].remove : @blocks[y][x].add
+render = lambda do
+    height.times do |y|
+      width.times do |x|
+        blocks[y][x].color = %w{ #158FAC #F1F101 #2FFF43 #DF0F0F #5858FF #FFB950 #FF98F3 }[field[y][x] - 1]
+        field[y][x].zero? ? blocks[y][x].remove : blocks[y][x].add
       end
     end
   end
 
-  def birth
-    @piece = Tetromino.new
-    fail "game over" if collision?
-    write_to_field
+piece = nil
+collision = nil
+write_to_field = nil
+birth = lambda do
+    piece = Tetromino.new
+    fail "game over" if collision.call
+    write_to_field.call
   end
 
-  def write_to_field
-    x, y = @piece.x, @piece.y
-    @piece.get.map.with_index do |row, dy|
+write_to_field = lambda do
+    x, y = piece.x, piece.y
+    piece.get.map.with_index do |row, dy|
       row.each_index do |dx|
-        @field[y + dy][x + dx] = row[dx] unless row[dx].zero?
+        field[y + dy][x + dx] = row[dx] unless row[dx].zero?
       end
     end
   end
 
-  def delete_from_field
-    x, y = @piece.x, @piece.y
-    @piece.get.map.with_index do |row, dy|
+delete_from_field = lambda do
+    x, y = piece.x, piece.y
+    piece.get.map.with_index do |row, dy|
       row.each_index do |dx|
-        @field[y + dy][x + dx] = 0 unless row[dx].zero?
+        field[y + dy][x + dx] = 0 unless row[dx].zero?
       end
     end
   end
 
-  def one_down
-    @key_in = false
-    delete_from_field
-    down_flag = false
-    @piece.y += 1
-    if collision?
-      @piece.y -= 1
-      down_flag = true
+one_down = lambda do
+    key_in = false
+    delete_from_field.call
+    t = false
+    piece.y += 1
+    if collision.call
+      piece.y -= 1
+      t = true
     end
-    write_to_field
-    @key_in = true
-    return down_flag
+    write_to_field.call
+    key_in = true
+    t
   end
 
-  def collision?
-    x, y = @piece.x, @piece.y
-    return true if y + @piece.height > Height || x + @piece.width > Width
-    @piece.get.map.each_with_index.any? do |row, dy|
+collision = lambda do
+    x, y = piece.x, piece.y
+    return true if y + piece.height > height || x + piece.width > width
+    piece.get.map.each_with_index.any? do |row, dy|
       row.map.each_with_index.any? do |a, dx|
-        a.nonzero? && @field[y + dy][x + dx].nonzero?
+        a.nonzero? && field[y + dy][x + dx].nonzero?
       end
     end
   end
 
-  def delete_blocks
-    Height.times.any? do |y|
-      next unless @field[y].all? &:nonzero?
-      @key_in = false
-      @field.delete_at y
-      @field.unshift Array.new Width, 0
+delete_blocks = lambda do
+    height.times.any? do |y|
+      next unless field[y].all? &:nonzero?
+      key_in = false
+      field.delete_at y
+      field.unshift Array.new width, 0
       true
     end
   end
 
-  def move dx
-    delete_from_field
-    @piece.x += dx
-    @piece.x -= dx if @piece.x < 0 || collision?
-    write_to_field
+move = lambda do |dx|
+    delete_from_field.call
+    piece.x += dx
+    piece.x -= dx if piece.x < 0 || collision.call
+    write_to_field.call
   end
 
-  def rotate
-    delete_from_field
-    @piece.rotate 1
-    @piece.rotate -1 if collision?
-    write_to_field
+rotate = lambda do
+    delete_from_field.call
+    piece.rotate 1
+    piece.rotate -1 if collision.call
+    write_to_field.call
   end
-end
 
 
-f = Field.new
+init.call
 t = 1
 check_delete = false
 down_flag = false
 key_lock = false
-wait = Wait
 
 on :key_down do |event|
-  next if key_lock || !f.key_in || down_flag
+  next if key_lock || !key_in || down_flag
   key_lock = true
   case event.key
-  when "left"  then f.move -1
-  when "right" then f.move 1
-  when "up"    then f.rotate
+  when "left"  then move.call -1
+  when "right" then move.call 1
+  when "up"    then rotate.call
   when "down"  then wait = 2
   end
   key_lock = false
 end
 
-on :controller_button_down do |event|
-  next if key_lock || !f.key_in || down_flag
-  key_lock = true
-  case event.button
-  when :left  then f.move -1
-  when :right then f.move 1
-  when :a     then f.rotate
-  when :down  then wait = 2
-  end
-  key_lock = false
-end
-
-f.birth
-f.render
+birth.call
+render.call
 
 update do
   key_lock = true
-  down_flag = f.one_down if (t % wait).zero? && !down_flag
+  down_flag = one_down.call if (t % wait).zero? && !down_flag
 
   if (check_delete || down_flag) && (t % 30).zero?
-    check_delete = f.delete_blocks
+    check_delete = delete_blocks.call
     unless check_delete
-      wait = Wait
+      wait = 18
       down_flag = false
-      f.birth
-      f.key_in = true
+      birth.call
+      key_in = true
     end
   end
 
   t += 1
-  f.render
+  render.call
   key_lock = false
 end
 
