19dbaa454b66b285fb7bb106cc794fd2f5c93638
Victor Maslov <nakilon@gmail.com>
2019-05-10 04:19:46 +0300

    dried the 'draw' routine; rearranged some pieces of code; empty cell is now nil, not zero

--- main.rb
+++ main.rb
@@ -2,31 +2,10 @@
 
 
 
-figure = num = nil
-get = lambda do
-  figure.map{ |row| row.map{ |i| i * num } }
-end
 x = y = nil
 
 width, height = 10, 20
-field = Array.new(height){ Array.new width, 0 }
-
-write_to_field = lambda do
-  get.call.map.with_index do |row, dy|
-    row.each_index do |dx|
-      next if row[dx].zero?
-      field[y + dy][x + dx] = row[dx]
-    end
-  end
-end
-delete_from_field = lambda do
-  get.call.map.with_index do |row, dy|
-    row.each_index do |dx|
-      next if row[dx].zero?
-      field[y + dy][x + dx] = 0
-    end
-  end
-end
+field = Array.new(height){ Array.new width }
 
 require "ruby2d"
 render = lambda do
@@ -49,27 +28,41 @@ render = lambda do
   lambda do
     height.times do |y|
       width.times do |x|
-        if field[y][x].zero?
-          blocks[y][x].remove
-        else
-          blocks[y][x].color = %w{ #158FAC #F1F101 #2FFF43 #DF0F0F #5858FF #FFB950 #FF98F3 }[field[y][x] - 1]
+        if field[y][x]
+          blocks[y][x].color = %w{ #158FAC #F1F101 #2FFF43 #DF0F0F #5858FF #FFB950 #FF98F3 }[(field[y][x] || 0) - 1]
           blocks[y][x].add
+        else
+          blocks[y][x].remove
         end
       end
     end
   end
 end.call
 
+figure = num = nil
+get = lambda do
+  figure.map{ |row| row.map{ |i| i * num } }
+end
+
 collision = lambda do
   return true if y + figure.      size > height
   return true if x + figure.first.size > width
-  get.call.map.with_index.any? do |row, dy|
-    row.map.each_with_index.any? do |a, dx|
-      a.nonzero? && field[y + dy][x + dx].nonzero?
+  get.call.each_with_index.any? do |row, dy|
+    row.each_with_index.any? do |a, dx|
+      !a.zero? && field[y + dy][x + dx]
     end
   end
 end
 
+
+draw = lambda do |f|
+  get.call.map.with_index do |row, dy|
+    row.each_index do |dx|
+      next if row[dx].zero?
+      field[y + dy][x + dx] = (row[dx] if f)
+    end
+  end
+end
 
 wait = 18
 
@@ -80,15 +73,15 @@ update do
   tick += 1
 
   if num && (tick % wait).zero?
-    delete_from_field.call
+    draw.call false
     y += 1
     unless collision.call
-      write_to_field.call
+      draw.call true
     else
       y -= 1
-      write_to_field.call
-      a, b = field.partition{ |row| row.none? &:zero? }
-      field = a.map{ Array.new width, 0 } + b
+      draw.call true
+      a, b = field.partition &:all?
+      field = a.map{ Array.new width } + b
       wait = 18
       num = nil
     end
@@ -109,17 +102,17 @@ update do
     x, y = 3, 0
 
     abort "game over" if collision.call
-    write_to_field.call
+    draw.call true
   end
   render.call
   key_lock = false
 end
 
 move = lambda do |dx|
-  delete_from_field.call
+  draw.call false
   x += dx
   x -= dx if x < 0 || collision.call
-  write_to_field.call
+  draw.call true
 end
 
 on :key_down do |event|
@@ -129,12 +122,12 @@ on :key_down do |event|
   when "left"  then move.call -1
   when "right" then move.call 1
   when "up"    then
-    delete_from_field.call
+    draw.call false
 
     figure = figure.reverse.transpose
     figure = figure.transpose.reverse if collision.call
 
-    write_to_field.call
+    draw.call true
   when "down"  then wait = 2
   end
   key_lock = false
