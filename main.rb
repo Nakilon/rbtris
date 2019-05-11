ce75ffeb3cadb1c492de317e6826ab1492fc467b
Victor Maslov <nakilon@gmail.com>
2019-05-12 00:35:18 +0300

    indentation

--- main.rb
+++ main.rb
@@ -77,54 +77,54 @@ end
 prev, row_time = nil, 0
 tap do
   points, first_time = 0, nil
-text_score = Text.new points, x: 5, y: 5, font: Font.path("PressStart2P-Regular.ttf")
-text_level = Text.new points, x: 5, y: 5, font: Font.path("PressStart2P-Regular.ttf")
-update do
-  current = Time.now
-  first_time ||= current
-  semaphore.synchronize do
-    text_score.text = "Score: #{points}"
-    level = (((points / 5 + 0.125) * 2) ** 0.5 - 0.5 + 1e-6).floor
-    text_level.text = "Level: #{level}"
-    text_level.x = Window.width - 5 - text_level.width
-    row_time = (0.8 - (level - 1) * 0.007) ** (level - 1)
-    prev ||= current - row_time
-    if current >= prev + row_time
-      prev += row_time
-      if figure
-        y += 1
-        unless collision.call
-          draw_state.call
-        else
-          y -= 1
-          mix.call true
-          a, b = field.partition &:all?
-          field = a.map{ Array.new field.first.size } + b
-          points += [0, 1, 3, 5, 8].fetch a.size
-          render.call
-          figure = nil
+  text_score = Text.new points, x: 5, y: 5, font: Font.path("PressStart2P-Regular.ttf")
+  text_level = Text.new points, x: 5, y: 5, font: Font.path("PressStart2P-Regular.ttf")
+  update do
+    current = Time.now
+    first_time ||= current
+    semaphore.synchronize do
+      text_score.text = "Score: #{points}"
+      level = (((points / 5 + 0.125) * 2) ** 0.5 - 0.5 + 1e-6).floor
+      text_level.text = "Level: #{level}"
+      text_level.x = Window.width - 5 - text_level.width
+      row_time = (0.8 - (level - 1) * 0.007) ** (level - 1)
+      prev ||= current - row_time
+      if current >= prev + row_time
+        prev += row_time
+        if figure
+          y += 1
+          unless collision.call
+            draw_state.call
+          else
+            y -= 1
+            mix.call true
+            a, b = field.partition &:all?
+            field = a.map{ Array.new field.first.size } + b
+            points += [0, 1, 3, 5, 8].fetch a.size
+            render.call
+            figure = nil
+          end
         end
       end
-    end
 
-    unless figure
-      figure = [
-        %w{ 1111    },
-        %w{ 22  22  },
-        %w{ 033 330 },
-        %w{ 440 044 },
-        %w{ 500 555 },
-        %w{ 006 666 },
-        %w{ 070 777 },
-      ].sample.map{ |st| st.chars.map &:to_i }
-      x, y = 3, 0
+      unless figure
+        figure = [
+          %w{ 1111    },
+          %w{ 22  22  },
+          %w{ 033 330 },
+          %w{ 440 044 },
+          %w{ 500 555 },
+          %w{ 006 666 },
+          %w{ 070 777 },
+        ].sample.map{ |st| st.chars.map &:to_i }
+        x, y = 3, 0
 
-      abort "#{text_score.text}\n#{text_level.text}" if collision.call
+        abort "#{text_score.text}\n#{text_level.text}" if collision.call
 
-      draw_state.call
+        draw_state.call
+      end
     end
   end
-end
 end
 
 
