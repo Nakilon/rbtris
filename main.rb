68be4c2b5e8bf6584714b4d9a989f31e8bdd983b
Victor Maslov <nakilon@gmail.com>
2019-05-21 19:59:19 +0300

    some refactoring around events to implement the scoreboard (don't ask me how it's related)

--- main.rb
+++ main.rb
@@ -9,31 +9,34 @@ unless File.exist? "PressStart2P-Regular.ttf"
   Zip::File.open(tempfile){ |zip| zip.extract "PressStart2P-Regular.ttf", "PressStart2P-Regular.ttf" }
 end
 
-points = figure = field = x = y = nil
+
+field = nil
+
 require "ruby2d"
-render = lambda do
-  block_margin = 1
-  block_side = 35
-  s = block_margin * 2 + block_side
-  margin = 30
+reset_field = lambda do
   field = Array.new(20){ Array.new 10 }
-  w = margin * 2 + s * field.first.size
-  h = margin * 2 + s * field.size
+end
+render = lambda do
+  margin = 1
+  inner = 30
+  s = inner + 2 * margin
+  reset_field.call
+  w = s * (2 + field.first.size)
+  h = s * (3 + field.size)
   set width: w, height: h, title: "rbTris"
-  Rectangle.new width: w,              height: h,              color: "gray"
-  Rectangle.new width: w - 2 * margin, height: h - 2 * margin, color: "black", x: margin, y: margin
+  Rectangle.new width: w,         height: h,         color: "gray"
+  Rectangle.new width: w - 2 * s, height: h - 3 * s, color: "black", x: s, y: s * 2
   blocks = Array.new(field.size) do |y|
     Array.new(field.first.size) do |x|
       [
-        Square.new(x: margin + block_margin + s * x,
-                   y: margin + block_margin + s * y,
-                   size: block_side),
+        Square.new(x: margin + s * (1 + x),
+                   y: margin + s * (2 + y),
+                   size: inner),
         nil
       ]
     end
   end
   lambda do
-    points, figure, field = 0, nil, Array.new(20){ Array.new 10 } unless points
     blocks.each_with_index do |row, i|
       row.each_with_index do |(block, drawn), j|
         if field[i][j]
@@ -53,19 +56,7 @@ render = lambda do
   end
 end.call
 
-collision = lambda do   # there is no collision
-  figure.each_with_index.all? do |row, dy|
-    row.each_with_index.all? do |a, dx|
-      a.zero? || (
-        ((0...field.size      ) === y + dy) &&
-        ((0...field.first.size) === x + dx) &&
-        !field[y + dy][x + dx]
-      )
-    end
-  end
-end
-
-
+figure = x = y = nil
 mix = lambda do |f|
   figure.each_with_index do |row, dy|
     row.each_index do |dx|
@@ -85,11 +76,48 @@ paused = false
 pause_rect = Rectangle.new(width: Window.width, height: Window.height, color: [0.5, 0.5, 0.5, 0.75]).tap &:remove
 pause_text = Text.new("press 'P'", z: 1, font: Font.path("PressStart2P-Regular.ttf")).tap &:remove
 
+
+points = nil
+collision = lambda do   # there is no collision
+  figure.each_with_index.all? do |row, dy|
+    row.each_with_index.all? do |a, dx|
+      a.zero? || (
+        ((0...field.size      ) === y + dy) &&
+        ((0...field.first.size) === x + dx) &&
+        !field[y + dy][x + dx]
+      )
+    end
+  end
+end
+text_score = Text.new points, x: 5, y: 5, z: 1, font: Font.path("PressStart2P-Regular.ttf")
+text_level = Text.new points, x: 5, y: 5, z: 1, font: Font.path("PressStart2P-Regular.ttf")
+init_figure = lambda do
+  figure = %w{ 070 777 006 666 500 555 440 044 033 330 22 22 1111 }.each_slice(2).to_a.sample
+  rest = figure.first.size - figure.size
+  figure = (
+    [?0 * figure.first.size] * (rest / 2) + figure +
+    [?0 * figure.first.size] * (rest - rest / 2)
+  ).map!{ |st| st.chars.map &:to_i }
+  x, y = 3, 0
+  next draw_state.call if collision.call
+  open("#{Dir.home}/.rbtris", "a") do |f|
+    f.puts "1 #{"#{text_level.text}   #{text_score.text}".tap &method(:puts)}"
+  end
+  [pause_rect, pause_text].each &((paused ^= true) ? :add : :remove)
+  points = nil
+end
+reset = lambda do
+  points, figure = 0, nil
+  reset_field.call
+  init_figure.call
+end
+
+
 prev, row_time = nil, 0
 tap do
   points, first_time = 0, nil
-  text_score = Text.new points, x: 5, y: 5, z: 1, font: Font.path("PressStart2P-Regular.ttf")
-  text_level = Text.new points, x: 5, y: 5, z: 1, font: Font.path("PressStart2P-Regular.ttf")
+  reset_field.call
+  init_figure.call
   update do
     current = Time.now
     first_time ||= current
@@ -115,25 +143,9 @@ tap do
             field = a.map{ Array.new field.first.size } + b
             points += [0, 1, 3, 5, 8].fetch a.size
             render.call
-            figure = nil
+            init_figure.call
           end
         end
-      end
-
-      unless figure
-        figure = %w{ 070 777 006 666 500 555 440 044 033 330 22 22 1111 }.each_slice(2).to_a.sample
-        rest = figure.first.size - figure.size
-        figure = (
-          [?0 * figure.first.size] * (rest / 2) + figure +
-          [?0 * figure.first.size] * (rest - rest / 2)
-        ).map!{ |st| st.chars.map &:to_i }
-        x, y = 3, 0
-        next draw_state.call if collision.call
-        open("#{Dir.home}/.rbtris", "a") do |f|
-          f.puts "1 #{"#{text_level.text}   #{text_score.text}".tap &method(:puts)}"
-        end
-        [pause_rect, pause_text].each &((paused ^= true) ? :add : :remove)
-        points = nil
       end
     end
   end
@@ -141,11 +153,13 @@ end
 
 
 try_move = lambda do |dir|
+  next unless figure
   x += dir
   next draw_state.call if collision.call
   x -= dir
 end
 try_up = lambda do
+  next unless figure
   figure = figure.reverse.transpose
   next draw_state.call if collision.call
   figure = figure.transpose.reverse
@@ -161,10 +175,13 @@ on :key_down do |event|
     when "left"  ; try_move[-1] unless paused
     when "right" ; try_move[+1] unless paused
     when "up"    ; try_up.call  unless paused
-    when "r" ; (points = nil ; render.call) unless paused
+    when "r" ; reset.call unless paused
     when "p", "space"
-      render.call unless points
       [pause_rect, pause_text].each &((paused ^= true) ? :add : :remove)
+      unless points
+        reset.call
+        draw_state.call
+      end
     end
   end
 end
