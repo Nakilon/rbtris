dc3a4a50c3631ae69f9bfa51b42b3724d6678958
kojix2 <2xijok@gmail.com>
2021-06-02 23:46:44 +0900

    use URI#open instead of Kernel#open

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ unless File.exist? "PressStart2P-Regular.ttf"
   require "open-uri"
   require "zip"
   tempfile = Tempfile.new "Press_Start_2P.zip"
-  File.binwrite tempfile, open("https://fonts.google.com/download?family=Press%20Start%202P", &:read)
+  File.binwrite tempfile, URI.open("https://fonts.google.com/download?family=Press%20Start%202P", &:read)
   Zip::File.open(tempfile){ |zip| zip.extract "PressStart2P-Regular.ttf", "PressStart2P-Regular.ttf" }
 end
 
